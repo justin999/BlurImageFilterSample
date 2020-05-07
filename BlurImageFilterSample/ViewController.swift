919606c7585b17e0a510f1cba8848946bfa01c44
Justin Sato <koichi.sato.aow@gmail.com>
2020-05-08 00:22:40 +0900

    apply Blur

--- BlurImageFilterSample/ViewController.swift
+++ BlurImageFilterSample/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        applyingGaussianBlur()
     }
 
+    private func applyingGaussianBlur() {
+        guard
+            let ciImage = CIImage(image: UIImage(named: "coco_singing")!)
+        else {
+            return
+        }
+        // Due to Core Image's coordinate system mismatch with UIKit, this filtering approach may yield unexpected results when displayed in a UIImageView with contentMode. Be sure to back it with a cgImage so that it handles contentMode properly.
+        // https://developer.apple.com/documentation/coreimage/ciimage/1437833-cropped
+        
+        let resultImage = ciImage.applyingGaussianBlur(sigma: 5)
+        let croppedImage = resultImage.cropped(to: ciImage.extent)
+        self.imageView.image = UIImage(ciImage: croppedImage)
+        
+        
+    }
 
 }
 
