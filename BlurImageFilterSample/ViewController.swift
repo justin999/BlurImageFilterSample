a647b7c631cd10a0114e5ff1c026949a22afd202
Justin Sato <koichi.sato.aow@gmail.com>
2020-05-09 22:38:19 +0900

    add home and metal section

--- BlurImageFilterSample/ViewController.swift
+++ BlurImageFilterSample/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
         applyingGaussianBlur()
     }
 
